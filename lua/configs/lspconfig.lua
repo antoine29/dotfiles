d72b921d5601d1a201450e70028741024d4dac49
anthony <floresgomezanthony@gmail.com>
2025-05-30 01:09:10 -0400

    lsp configs for go and python

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,25 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup {
+    ensure_installed = { 
+        "gopls",
+        "dockerls",
+        "marksman",
+        "pyright",
+    },
+}
+
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup {
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,25 +4,37 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
+  
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  		  "vim", "lua", "vimdoc",
+        "html", "css",
+        "go",
+        "python",
+        "dockerfile",
+        "markdown", "markdown_inline",
+        "json", "yaml",
+        "bash", "regex",
+        "javascript", "typescript",
+  		},
+  	},
+  },
 }
