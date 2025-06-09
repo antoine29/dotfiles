0627c2d63e04731ee504000248a2bda89bebc6e6
anthony <floresgomezanthony@gmail.com>
2025-06-08 23:24:30 -0400

    set ups copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,7 @@ return {
       require "configs.lspconfig"
     end,
   },
-  
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
@@ -37,6 +37,20 @@ return {
   		},
   	},
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      -- { "github/copilot.vim" }, -- commenting this disables the automatic suggestions. chat should keep available though
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    event = "VimEnter", -- This will load the plugin when Vim starts
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   require("configs.debug"),
 }
+
