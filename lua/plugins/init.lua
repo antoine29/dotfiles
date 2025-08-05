f0a32abfce6020dd792309adc2f5c000d7ad9399
Anthony Flores <Anthony.Flores2@pfizer.com>
2025-08-05 14:20:59 -0400

    copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,14 +41,16 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      -- { "github/copilot.vim" }, -- commenting this disables the automatic suggestions. chat should keep available though
+      -- { "github/copilot.vim" }, -- commenting this disables the automatic suggestions. chat should keep available though. uncomment and run Copilot auth to setup copilot login
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     event = "VimEnter", -- This will load the plugin when Vim starts
     opts = {
       -- See Configuration section for options
+      model = "claude-3.5-sonnet",
     },
+    sticky = {'#buffers', '#files:*'},
     -- See Commands section for default commands if you want to lazy load on them
   },
   {
